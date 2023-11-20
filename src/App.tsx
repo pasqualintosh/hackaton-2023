49fb7fc42afe9bb84ea7ac49456b32d6222ed56d
Noah Giesel <giesel@extendi.it>
2023-11-20 16:47:08 +0100

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Map } from './components';
+import { Map } from "./components";
+import { Landing } from "./components/landing";
 
 export default function App() {
   return (
     <div>
       <Map />
-      <div className='absolute top-[172px] left-[30px] w-[40vw] h-[60vh] bg-primary-default rounded-lg'>
+      <div className="absolute top-[172px] left-[30px] w-[40vw] h-[60vh] bg-primary-default rounded-lg">
         {/* <input
           type='text'
           className='border-gray-200 border-1 rounded-2xl bg-primary-default'
         /> */}
       </div>
+      <Landing />
     </div>
   );
 }
---
+++ src/types/ColorType.ts
@@ -0,0 +1,6 @@
+export type ColorKeys =
+  | "dark-purple"
+  | "light-purple"
+  | "white"
+  | "transparent";
+export type ColorType = { [k in ColorKeys]: string };
